404b49cb96cb947ca06e739b542d1e5afe6b3b9a
mendozam1335 <55033050+mendozam1335@users.noreply.github.com>
2025-12-29 12:35:27 -0600

    Implement new camera system

--- water-warfare/Scripts/Camera.gd
+++ water-warfare/Scripts/Camera.gd
@@ -17,6 +17,10 @@ var aim_speed: float = 0.2
 @export var aim_shoulder_offset: float = 0.5
 @export var aim_fov: float = 55
 
+@export var sprint_fov: float = 100
+@export var spring_distance: float = 1.8
+@export var sprint_tween_speed: float = 0.5
+
 @export var default_rear_distance := 1.5
 @export var default_shoulder_offset := 1.0
 @export var default_fov: float = 75
@@ -26,6 +30,8 @@ var aim_speed: float = 0.2
 
 var camera_tween: Tween
 var shoulder_sign: float = 1.0
+var horizontal: float = 0.0
+var vertical: float = 0.0
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -41,14 +47,7 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-		#if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		#else:
-			#Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-		if event is InputEventMouseMotion:
-			var mouse_event: Vector2 = event.screen_relative * mouse_sensitivity
-			camera_look(mouse_event)
-			
+		
 	if event.is_action_pressed("camera_switch"):
 		shoulder_sign *= -1.0
 		swap_camera_alignment()
@@ -59,58 +58,64 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_released("aim"):
 		exit_aim()
 	
-#func _unhandled_input(event: InputEvent) -> void:
-	#if event.is_action_pressed("camera_switch"):
-		#shoulder *= -1.0
-		#_apply_shoulder()
+	if event.is_action_pressed("sprint"):
+		enter_sprint()
+	
+	if event.is_action_released("sprint"):
+		exit_sprint()
+		
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.mouse_mode != Input.MOUSE_MODE_CAPTURED:
+		return
+	if event is InputEventMouseMotion:
+		camera_rotation += event.screen_relative * mouse_sensitivity
+		camera_look()
 
-func camera_look(mouse_movement: Vector2) -> void:
-	camera_rotation += mouse_movement
+func camera_look() -> void:
+	horizontal = wrapf(horizontal - camera_rotation.x, -PI, PI)
+	vertical = clampf(vertical - camera_rotation.y, min_boundary, max_boundary)
 	
-	transform.basis = Basis()
-	player.transform.basis = Basis()
-	
-	camera_rotation.y = clampf(
-		camera_rotation.y,
-		min_boundary,
-		max_boundary
-	)
-	
-	player.rotate_object_local(Vector3(0,1,0), -camera_rotation.x)
-	rotate_object_local(Vector3(1,0,0), -camera_rotation.y)
-
-#func _handle_aim(delta: float) -> void:
-	#var target_dist := aim_distance if is_aiming else desired_distance
-	#smooth_camera.spring_length = lerp(smooth_camera.spring_length, target_dist, 1.0 - exp(-10.0 *delta))
-#
-#func _apply_shoulder() -> void:
-	#smooth_camera.position.x = default_shoulder_offset * shoulder
+	player.rotation.y = horizontal
+	rotation.x = vertical
+	camera_rotation = Vector2.ZERO
 
 func swap_camera_alignment() -> void:
-	set_rear_spring_arm_position(default_shoulder_offset * shoulder_sign, camera_alignment_speed)
-	
-func set_rear_spring_arm_position(pos: float, speed: float) -> void:
-	if camera_tween:
-		camera_tween.kill() 
-	camera_tween = get_tree().create_tween()
-	camera_tween.tween_property(edge_spring_arm, "spring_length", pos, speed)
-	
-func enter_aim() ->void:
+	tween_setup()
+	camera_tween.tween_property(
+		edge_spring_arm, 
+		"spring_length", 
+		default_shoulder_offset * shoulder_sign, 
+		camera_alignment_speed
+		)
+
+func tween_setup() -> void:
 	if camera_tween:
 		camera_tween.kill()
 	camera_tween = get_tree().create_tween()
 	camera_tween.set_parallel()
+	camera_tween.set_trans(Tween.TRANS_EXPO)
+	camera_tween.set_ease(Tween.EASE_OUT)
+	
+func enter_aim() ->void:
+	tween_setup()
 	
 	camera_tween.tween_property(camera, "fov", aim_fov, aim_speed)
 	camera_tween.tween_property(edge_spring_arm, "spring_length", aim_shoulder_offset * shoulder_sign, aim_speed)
 	camera_tween.tween_property(rear_spring_arm, "spring_length", aim_distance, aim_speed)
 
 func exit_aim() -> void:
-	if camera_tween:
-		camera_tween.kill()
-	camera_tween = get_tree().create_tween()
-	camera_tween.set_parallel()
+	tween_setup()
 	
 	camera_tween.tween_property(camera, "fov", default_fov, aim_speed)
 	camera_tween.tween_property(edge_spring_arm, "spring_length", default_shoulder_offset * shoulder_sign, aim_speed)
 	camera_tween.tween_property(rear_spring_arm, "spring_length", default_rear_distance, aim_speed)
+	
+func enter_sprint() -> void:
+	tween_setup()
+	
+	camera_tween.tween_property(camera, "fov", sprint_fov, sprint_tween_speed)
+	
+func exit_sprint() -> void:
+	tween_setup()
+	
+	camera_tween.tween_property(camera, "fov", default_fov, sprint_tween_speed)
--- water-warfare/Scripts/player.gd
+++ water-warfare/Scripts/player.gd
@@ -4,25 +4,10 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
-var _look := Vector2.ZERO
-var mouse_moved: bool = false
-
-@export var mouse_sensitivity: float = 0.00075
-@export var decay: float = 20.0
-@export var min_boundary: float = -60
-@export var max_boundary: float = 10
-
-@onready var horizontal_pivot: Node3D = $HorizontalPivot
-@onready var vertical_pivot: Node3D = $HorizontalPivot/VerticalPivot
-@onready var camera_rig: Node3D = $CameraRig
 @onready var rig_pivot: Node3D = $RigPivot
 
 
-#func _ready() -> void:
-	#Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-
 func _physics_process(delta: float) -> void:
-	#frame_camera_rotation()
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -36,62 +21,14 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
-		#look_toward_direction(direction, delta)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 	
-	#if mouse_moved:
-	#	look_toward_direction(direction,delta)
-	#	mouse_moved = false
 	move_and_slide()
 
-#func _unhandled_input(event: InputEvent) -> void:
-	#if event.is_action_pressed("ui_cancel"):
-		#Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-		
-	#todo: toggle camera movement for shoulder placment left or right
-	#if event.is_action_pressed("aim"):
-		#print("currently aiming")
-		##camera_rig.is_aiming = true
-		#
-	#if event.is_action_released("aim"):
-		#print("no longer aiming")
-		#camera_rig.is_aiming = false
-		
-	#if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-		#if event is InputEventMouseMotion:
-			#_look += -event.relative * mouse_sensitivity
-			#mouse_moved = true
-			
 
 func getMoveDirection() -> Vector3:
 	var input_dir:= Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	#var input_vector:= Vector3(input_dir.x, 0, input_dir.y).normalized()
 	return (transform.basis * Vector3(input_dir.x , 0, input_dir.y)).normalized()
-
-#rotates the pivots but not the actual camera. Camera is set to pivot point in Smooth_camera script. 
-func frame_camera_rotation() -> void:
-	horizontal_pivot.rotate_y(_look.x)
-	vertical_pivot.rotate_x(_look.y)
-	
-	#Make sure camera cannot go over head or underskirt
-	vertical_pivot.rotation.x = clampf(
-		vertical_pivot.rotation.x,
-		deg_to_rad(min_boundary),
-		deg_to_rad(max_boundary)
-	)
-	
-	_look = Vector2.ZERO
-	
-func look_toward_direction(direction: Vector3, delta: float) -> void:
-	var target_transform := rig_pivot.global_transform.looking_at(
-		rig_pivot.global_position + direction, 
-		Vector3.UP,
-		true
-	)
-	
-	rig_pivot.global_transform = rig_pivot.global_transform.interpolate_with(
-		target_transform,
-		1.0 - exp(-decay * delta)
-	)
